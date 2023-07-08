079e33a6dd43ec1e230dc754fba23bb6cbf4ce46
Vatibor <vatibor2@gmail.com>
2023-07-08 19:25:30 +0200

    # Modify style typo

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -5,7 +5,7 @@ import PromptCard from "@components/PromptCard";
 
 const PromptCardList = ({ data, handleTagClick}) => {
   return (
-    <div className="mt-16 prompt-layout">
+    <div className="mt-16 prompt_layout">
       {data.map((post) => (
         <PromptCard
           key={post._id}
@@ -19,23 +19,52 @@ const PromptCardList = ({ data, handleTagClick}) => {
 
 const Feed = () => {
   const [searchText, setSearchText] = useState("")
-  const [posts, setPosts] = useState([]);
-  const handleSearchChange = (e) => {
+  const [searchTimeout, setSearchTimeout] = useState(null)
+  const [searchedResults, setSearchedResults] = useState([]);
 
+  const [allPosts, setAllPosts] = useState([]);
+
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt")
+    const data = await response.json()
+
+    setAllPosts(data)
+  }
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout)
+    setSearchText(e.target.value)
+
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value)
+        setSearchedResults(searchResult)
+      }, 500)
+    )
   }
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await fetch("/api/prompt")
-      const data = await response.json()
-
-      setPosts(data)
-    }
-
     fetchPosts()
   }, []);
-  
-  
+
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
+    return allPosts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+  };
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName)
+
+    const searchResult = filterPrompts(tagName)
+    setSearchedResults(searchResult)
+  }
+
   return (
     <section className="feed">
       <form className="relative w-full flex-center">
@@ -49,10 +78,16 @@ const Feed = () => {
         />
       </form>
 
-      <PromptCardList
-        data={posts}
-        handleTagClick={() => {}}
-      />
+      {/* All Prompts*/}
+      {searchText ? (
+        <PromptCardList
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        />
+      ) : (
+        <PromptCardList data={allPosts} handleTagClick={handleTagClick} />
+      )}
+
     </section>
   );
 };
